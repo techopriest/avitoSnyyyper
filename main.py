e2148e74dee966430ca5ed08f279f0f2cc40b86a
techopriest <techopriest@gmail.com>
2023-08-03 22:39:19 +1000

    create rep

---
+++ main.py
@@ -0,0 +1,19 @@
+import undetected_chromedriver as uc
+driver = uc.Chrome()
+driver.get('https://www.avito.ru/habarovsk/bytovaya_elektronika')
+
+class avitoParse:
+    def __init__(self) -> None:
+        pass
+
+    def set_up(self):
+        pass
+
+    def paginator(self):
+        pass
+
+    def parse_page(self):
+        pass
+
+    def parse(self):
+        pass
